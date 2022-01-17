8ce23085a13f096a53dab850d0a5b8c306a0a1e0
G0ldsam <vagosxristof@gmail.com>
2022-01-18 01:05:02 +0200

    initial commit

---
+++ EventFool/Validations/DateGreaterThanAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EventFool.Validations
+{
+    public class DateGreaterThanAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime _date = Convert.ToDateTime(value);
+            if (_date >= DateTime.Now)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Please enter a valid start date.");
+            }
+        }
+    }
+}
